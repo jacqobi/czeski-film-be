32c88fc1f97cbaba6e68d1a142529b78d6746873
nickmatsnev <matsnev2001@yandex.ru>
2025-11-15 22:41:35 +0100

    adds imports

--- devsmart/routes/api/admin/v1/__init__.py
+++ devsmart/routes/api/admin/v1/__init__.py
@@ -1,3 +1,7 @@
 from flask import Blueprint
 
 admin_bp = Blueprint('admin', __name__, url_prefix='/api/admin')
+
+from .improve import * 
+from .override import * 
+from .models import * 
--- devsmart/routes/api/user/v1/__init__.py
+++ devsmart/routes/api/user/v1/__init__.py
@@ -1,3 +1,5 @@
 from flask import Blueprint
 
 user_bp = Blueprint('user', __name__, url_prefix='/api/user')
+
+from .completion import * 
